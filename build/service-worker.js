22390602418a77da5b03ce9b724432e2fd5de494
Mercy <zql2828288@163.com>
2020-07-01 10:12:15 +0800

    改为hash路由

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3485de3c86427b1b3070dbd045941406.js"
+  "./precache-manifest.7072185dd76ebed243f3a3b2774b266a.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/config/history.ts
+++ src/config/history.ts
@@ -1,17 +1,22 @@
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history'
 
-const ENV = process.env.NODE_ENV;
-let publicUrl:string = '';
-
-if(ENV === 'development'){
-	publicUrl = '/';
-}else if(ENV === 'production'){
-	publicUrl = '/tomato-todo';
-}
-
-
-const history = createBrowserHistory({
-	basename: publicUrl
-});
+const history = createHashHistory()
 
 export default history
+// import { createBrowserHistory } from 'history';
+
+// const ENV = process.env.NODE_ENV;
+// let publicUrl:string = '';
+
+// if(ENV === 'development'){
+// 	publicUrl = '/';
+// }else if(ENV === 'production'){
+// 	publicUrl = '/tomato-todo';
+// }
+
+
+// const history = createBrowserHistory({
+// 	basename: publicUrl
+// });
+
+// export default history
